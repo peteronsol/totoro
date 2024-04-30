4e03e6d313dcf45479fa3ddd4f345d46cd14d95b
peteronsol <bids.crypto@gmail.com>
2024-04-30 14:04:53 +0530

    totoro coming sooon

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import img from "./assets/comingsoon-removebg.png";
+function App() {
+  return (
+    <>
+      <img src={img} alt="My Image" className="comingsoon" />
+    </>
+  );
+}
+
+export default App;
